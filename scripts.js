4f69585197fe42b536736ab0029d7520d5591205
PJ Jones <pj.jones4000.com>
2022-01-25 12:48:47 -0600

    think I'm finished with the lab

--- scripts.js
+++ scripts.js
@@ -1,29 +1,48 @@
-let text = 'PJ Jones';
+let text = "PJ Jones";
 let capName = text.toUpperCase();
-console.log('Name: ' + capName);
+console.log("Name: " + capName);
 
-console.log('Career: Web Developer');
-console.log('Description: Looking for work in Web Development');
+console.log("Career: Web Developer");
+console.log("Description: Looking for work in Web Development");
 
-console.log('My Interests:');
-console.log('* Drumming');
-console.log('* Meditating');
-console.log('* Gardening');
-console.log('* Playing Tetris');
+console.log("My Interests:");
+console.log("* Drumming");
+console.log("* Meditating");
+console.log("* Gardening");
+console.log("* Playing Tetris");
 
 let displayPosition = (company, title, description) => {
-console.log(`* ${title} at ${company} - ${description}.`)
+  console.log(`* ${title} at ${company} - ${description}.`);
 };
 
-console.log('My Previous Experience:');
-displayPosition('Trattoria Zaza', 'General Manager', 'Ran many aspects of the restaurant');
-displayPosition('Uber', 'Driver', 'Drove passengers to destination while ensuring safety');
-displayPosition('Henderson & Walton', 'Billing associate', 'Entered patient billing information');
+console.log("My Previous Experience:");
+displayPosition(
+  "Trattoria Zaza",
+  "General Manager",
+  "Ran many aspects of the restaurant"
+);
+displayPosition(
+  "Uber",
+  "Driver",
+  "Drove passengers to destination while ensuring safety"
+);
+displayPosition(
+  "Henderson & Walton",
+  "Billing associate",
+  "Entered patient billing information"
+);
 
-console.log('My Skills:');
+console.log("My Skills:");
 
-let displaySkill = (skill, Boolean) => {
-
+let displaySkill = (skill, cool) => {
+  if (cool) {
+    console.log(`* BAM: ${skill}`);
+  } else {
+    console.log(`* ${skill}`);
+  }
 };
-
-
+displaySkill('Drumming', true);
+displaySkill('Programming', true);
+displaySkill('Tetris', false);
+displaySkill('Composting', false);
+displaySkill('Drawing', true);
