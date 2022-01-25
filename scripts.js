e39835d5c94e7c581c64ae22d955fc2f701a36b9
PJ Jones <pj.jones4000.com>
2022-01-25 11:45:46 -0600

    working on syntax for boolen in displaySkill func

---
+++ scripts.js
@@ -0,0 +1,29 @@
+let text = 'PJ Jones';
+let capName = text.toUpperCase();
+console.log('Name: ' + capName);
+
+console.log('Career: Web Developer');
+console.log('Description: Looking for work in Web Development');
+
+console.log('My Interests:');
+console.log('* Drumming');
+console.log('* Meditating');
+console.log('* Gardening');
+console.log('* Playing Tetris');
+
+let displayPosition = (company, title, description) => {
+console.log(`* ${title} at ${company} - ${description}.`)
+};
+
+console.log('My Previous Experience:');
+displayPosition('Trattoria Zaza', 'General Manager', 'Ran many aspects of the restaurant');
+displayPosition('Uber', 'Driver', 'Drove passengers to destination while ensuring safety');
+displayPosition('Henderson & Walton', 'Billing associate', 'Entered patient billing information');
+
+console.log('My Skills:');
+
+let displaySkill = (skill, Boolean) => {
+
+};
+
+
